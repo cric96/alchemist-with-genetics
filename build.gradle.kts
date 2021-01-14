ad246cbf950a0795f04e58044c958fa0932586be
Danilo Pianini <danilo.pianini@unibo.it>
2021-01-14 14:32:42 +0100

    Better document build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,29 +41,38 @@ val runAll by tasks.register<DefaultTask>("runAll") {
  * Scan the folder with the simulation files, and create a task for each one of them.
  */
 File(rootProject.rootDir.path + "/src/main/yaml").listFiles()
-    .filter { it.extension == "yml" }
-    .sortedBy { it.nameWithoutExtension }
+    .filter { it.extension == "yml" } // pick all yml files in src/main/yaml
+    .sortedBy { it.nameWithoutExtension } // sort them, we like reproducibility
     .forEach {
+        // one simulation file -> one gradle task
         val task by tasks.register<JavaExec>("run${it.nameWithoutExtension.capitalize()}") {
-            group = alchemistGroup
-            description = "Launches simulation ${it.nameWithoutExtension}"
-            main = "it.unibo.alchemist.Alchemist"
-            classpath = sourceSets["main"].runtimeClasspath
+            group = alchemistGroup // This is for better organization when running ./gradlew tasks
+            description = "Launches simulation ${it.nameWithoutExtension}" // Just documentation
+            main = "it.unibo.alchemist.Alchemist" // The class to launch
+            classpath = sourceSets["main"].runtimeClasspath // The classpath to use
+            // In case our simulation produces data, we write it in the following folder:
             val exportsDir = File("${projectDir.path}/build/exports/${it.nameWithoutExtension}")
             doFirst {
+                // this is not executed upfront, but only when the task is actually launched
+                // If the export folder doesn not exist, create it and its parents if needed
                 if (!exportsDir.exists()) {
                     exportsDir.mkdirs()
                 }
             }
+            // These are the program arguments
             args("-y", it.absolutePath, "-e", "$exportsDir/${it.nameWithoutExtension}-${System.currentTimeMillis()}")
             if (System.getenv("CI") == "true" || batch == "true") {
+                // If it is running in a Continuous Integration environment, use the "headless" mode of the simulator
+                // Namely, force the simulator not to use graphical output.
                 args("-hl", "-t", maxTime)
             } else {
+                // A graphics environment should be available, so load the effects for the UI from the "effects" folder
+                // Effects are expected to be named after the simulation file
                 args("-g", "effects/${it.nameWithoutExtension}.aes")
             }
+            // This tells gradle that this task may modify the content of the export directory
             outputs.dir(exportsDir)
         }
-        // task.dependsOn(classpathJar) // Uncomment to switch to jar-based cp resolution
+        // task.dependsOn(classpathJar) // Uncomment to switch to jar-based classpath resolution
         runAll.dependsOn(task)
     }
-
